56adf37469e6a094cf48693b3c82e2c6f094a7a1
Tara <tsalli@greenwichacademy.org>
2022-11-03 04:26:37 -0400

    made AL of least diverse school districts

--- StudentAnalysisCode.java
+++ StudentAnalysisCode.java
@@ -7,19 +7,22 @@ public class StudentAnalysisCode {
     public static double totalNumNativeEnglishStudents;
     public static double totalNumTotalStudents;
 
+    public static String mostDiverseDistrict;
+    public static ArrayList<String> leastDiverseDistricts = new ArrayList<>();
+
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        File f = new File("cmtresults (1).csv");
+        File f = new File("SchoolEquitableRank.csv");
         File m = new File ("studentenrollmentbyell2008-2021.csv");
         Scanner cmtResultsFileScan = new Scanner(f);
-        Scanner languageStudentsFileScan = new Scanner(m);
-        //getTotalNumStudents(languageStudentsFileScan);
-        getNumStudentsByDistrict(languageStudentsFileScan);
+        getTotalNumStudents(m);
+        getHighestDistrict(m);
     }
 
 
-    public static void getTotalNumStudents(Scanner fileScan) throws FileNotFoundException {
+    public static void getTotalNumStudents(File m) throws FileNotFoundException {
+        Scanner fileScan = new Scanner(m);
         fileScan.nextLine();
         String[] header = fileScan.next().split(",");
         ArrayList<String> headerValues = new ArrayList<>(Arrays.asList(header));
@@ -48,6 +51,8 @@ public class StudentAnalysisCode {
         totalNumELLStudents = numEllStudents;
         totalNumNativeEnglishStudents = numNonEllStudents;
         totalNumTotalStudents = numEllStudents + numNonEllStudents;
+        System.out.println("Connecticut state Ell student population: " + calcualateEllPercentage(totalNumELLStudents, totalNumTotalStudents) + "%\n"+ 
+        "Connecticut state non-ELL student population: " + calcualateNonEllPercentage(totalNumNativeEnglishStudents, totalNumTotalStudents) + "%");
     }
 
 
@@ -60,10 +65,68 @@ public class StudentAnalysisCode {
     }
 
 
-    public static void getNumStudentsByDistrict(Scanner fileScan) throws FileNotFoundException {
+    public static void getHighestDistrict(File m) throws FileNotFoundException {
+        Scanner fileScan = new Scanner(m);
         fileScan.nextLine();
         ArrayList <String> districts = new ArrayList<>();
-        String mostDiverseDistrict = "";
+        //mostDiverseDistrict = "";
+        int greatestPercentage = 0;
+        int lowestPercentage = 0;
+        double totalStudents = 0.0;
+        double districtNumELLStudents = 0.0;
+        double districtNumNativeEnglishStudents = 0.0;
+        double districtNumTotalStudents = 0.0;
+        String[] header = fileScan.next().split(",");
+        ArrayList<String> headerValues = new ArrayList<>(Arrays.asList(header));
+        int yearIndex = headerValues.indexOf("Year");
+        int numStudentsIndex = headerValues.indexOf("Value");
+        int typeOfLearner = headerValues.indexOf("Learner");
+        int districtIndex = headerValues.indexOf("District");
+        Double numEllStudents = 0.0;
+        Double numNonEllStudents = 0.0;
+        while (fileScan.hasNextLine()) {
+            String line = fileScan.nextLine();
+            if (!line.equals("")) {
+                String[] line1 = line.split(",");
+                ArrayList<String> lineAsAL = new ArrayList<>(Arrays.asList(line1));
+                if(lineAsAL.get(yearIndex).equals("2020-2021") && Double.parseDouble(lineAsAL.get(numStudentsIndex))>0) {
+                    if (lineAsAL.get(typeOfLearner).equals("Non-English Language Learner")) {
+                        numNonEllStudents = Double.parseDouble(lineAsAL.get(numStudentsIndex));
+                        String[] line2 = fileScan.nextLine().split(",");
+                        ArrayList<String> lineAsAL2 = new ArrayList<>(Arrays.asList(line2));
+                        numEllStudents = Double.parseDouble(lineAsAL2.get(numStudentsIndex));
+                        totalStudents = numNonEllStudents + numEllStudents;
+                    } else {
+                    fileScan.nextLine();
+                }
+                
+                 if (calcualateEllPercentage(numEllStudents, totalStudents) > greatestPercentage) {
+                    greatestPercentage = calcualateEllPercentage(numEllStudents, totalStudents);
+                    mostDiverseDistrict = lineAsAL.get(districtIndex);
+                } 
+                else if (calcualateEllPercentage(numEllStudents, totalStudents) <= lowestPercentage) {
+                    lowestPercentage = calcualateEllPercentage(numEllStudents, totalStudents);
+                    leastDiverseDistricts.add(lineAsAL.get(districtIndex));
+                }
+            } else {
+                fileScan.nextLine();
+            }
+        }
+    }
+        System.out.println("The most lingusitically diverse school district in Connecticut (as of 2020-2021) is: " + mostDiverseDistrict  + 
+    ", where " + greatestPercentage + "% of the student population consists of ELL students.");
+
+    System.out.println("The least lingusitically diverse school districts in Connecticut (as of 2020-2021) are: " + leastDiverseDistricts  + 
+    ", where " + lowestPercentage + "% of the student populations consist of ELL students.");
+    }
+
+
+
+    public static void getDistrictEquitableValue(File m, String mostDiverseDistrict) throws FileNotFoundException {
+        Scanner fileScan = new Scanner(m);
+        fileScan.nextLine();
+        ArrayList <String> districts = new ArrayList<>();
+        //mostDiverseDistrict = "";
         int greatestPercentage = 0;
         double totalStudents = 0.0;
         double districtNumELLStudents = 0.0;
@@ -86,14 +149,9 @@ public class StudentAnalysisCode {
                     if (lineAsAL.get(typeOfLearner).equals("Non-English Language Learner")) {
                         numNonEllStudents = Double.parseDouble(lineAsAL.get(numStudentsIndex));
                         String[] line2 = fileScan.nextLine().split(",");
-                        System.out.println(Arrays.toString(line2));
                         ArrayList<String> lineAsAL2 = new ArrayList<>(Arrays.asList(line2));
                         numEllStudents = Double.parseDouble(lineAsAL2.get(numStudentsIndex));
-                        System.out.println(lineAsAL2.get(districtIndex) + " ell:" + numEllStudents);
                         totalStudents = numNonEllStudents + numEllStudents;
-                        System.out.println(totalStudents);
-                        System.out.println("percentage: " +calcualateEllPercentage(numEllStudents, totalStudents));
-                
                     } else {
                     fileScan.nextLine();
                 }
@@ -107,10 +165,8 @@ public class StudentAnalysisCode {
             }
         }
     }
-        System.out.println(totalStudents);
-        System.out.println(numNonEllStudents);
-        System.out.println(numEllStudents);
         System.out.println("The most lingusitically diverse school district in Connecticut (as of 2020-2021) is: " + mostDiverseDistrict  + 
     ", where " + greatestPercentage + "% of the student population consists of ELL students.");
     }
+
 }
